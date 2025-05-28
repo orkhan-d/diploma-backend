22ab36cc9332a98be17a81461e8e69da35c6ba39
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-05-28 18:08:18 +0300

    fix: fix blocks checking

--- app/handlers/message.py
+++ app/handlers/message.py
@@ -14,10 +14,14 @@ async def handle_message(message: types.Message):
     bot_token = message.bot.token
     # get json file from bots folder like bots/bot_token.json
     scheme = json.load(open(os.path.join('bots', f'{bot_token}.json'), 'r'))
-    scheme = scheme['blocks']['scheme']
+    blocks = scheme['blocks']
 
-    conditions = scheme.get('conditions', [])
-    actions = scheme.get('actions', [])
+    for block in blocks:
+        if block['block_types']['system_name'] == 'new_message':
+            block_scheme = block.get('scheme', {})
+            conditions = block_scheme.get('conditions', [])
+            actions = block_scheme.get('actions', [])
 
-    if handle_conditions(conditions, message=message):
-        await handle_actions(actions, message.bot, message=message)
+            if handle_conditions(conditions, message=message):
+                await handle_actions(actions, message.bot, message=message)
+                return
